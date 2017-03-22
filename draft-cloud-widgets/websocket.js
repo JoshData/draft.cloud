56798f69871e53fdd3c9b20cdeec592aaa661928
Joshua Tauberer <jt@occams.info>
2017-03-22 16:45:00 -0400

    only create one websocket on the client even if multiple documents are open

--- draft-cloud-widgets/websocket.js
+++ draft-cloud-widgets/websocket.js
@@ -1,22 +1,31 @@
 exports.name = "Websocket";
 
+var socket = null;
+
 exports.open = function(owner_name, document_name, api_key, cbobj) {
-  var socket = global.io.connect('/');
+  if (!socket) {
+    socket = global.io.connect('/');
+    socket.on('message', function (message) {
+      alert(message)
+    });
+    socket.streamcount = 0;
+  }
 
   var document_id;
   var access_level;
 
-  socket.on('message', function (message) {
-    alert(message)
-  });
+  var requestid = ++socket.streamcount;
 
   socket.emit('open-document', {
+    requestid: requestid,
     owner: owner_name,
     document: document_name,
     api_key: api_key
   });
 
   socket.on('document-opened', function (data) {
+    if (data.requestid != requestid) return; // for a different open-document request
+
     document_id = data.document.id;
     access_level = data.access_level;
 
@@ -35,7 +44,7 @@ exports.open = function(owner_name, document_name, api_key, cbobj) {
   });
 
   socket.on('new-revisions', function (data) {
-    // TODO: Check data.document == document_id
+    if (data.document != document_id) return; // for a different open-document stream
     cbobj.pull(data.revisions);
   });
 
@@ -52,12 +61,14 @@ exports.open = function(owner_name, document_name, api_key, cbobj) {
     });
   }    
 
-  socket.on('document-patch-received', function (revision) {
-    if (revision.error) {
-      cb("There was an error submitting a local change: " + revision.error);
+  socket.on('document-patch-received', function (data) {
+    if (data.document != document_id) return; // for a different open-document stream
+
+    if (data.error) {
+      cb("There was an error submitting a local change: " + data.error);
       return;
     }
 
-    current_push_cb(null, revision);
+    current_push_cb(null, data.revision);
   });
 }
