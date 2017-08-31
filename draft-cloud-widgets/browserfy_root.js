19f309008584d62b20fe3df7121cd3245e942655
Joshua Tauberer <jt@occams.info>
2017-08-31 08:44:58 -0400

    try using jsoneditor as a widget

--- draft-cloud-widgets/browserfy_root.js
+++ draft-cloud-widgets/browserfy_root.js
@@ -4,58 +4,49 @@
 // browserify -d draft-cloud-widgets/browserfy_root.js -o public_html/draftdotcloud.js
 //
 
-// Let other initialization occur before we actually start initialization.
-setTimeout(init, 0);
+// Add LINK/SCRIPT tags.
+var jsoneditor = require("./jsoneditor.js").jsoneditor;
+
+// Let other initialization occur before we actually start initialization,
+// including enough time to load the JSONEditor CSS & JS.
+setTimeout(init, 100);
 
 var log_receivers = [];
 
 function init() {
-  //var channel = require("./ajax_polling.js");
-  var channel = require("./websocket.js");
+  // Activate any elements with data-draftdotcloud-{owner,document,apikey}
+  // attributes.
+  var elements = document.getElementsByClassName("draftdotcloud-widget");
+  for (var i = 0; i < elements.length; i++) {
+    var elem = elements[i];
 
-  var textareas = document.getElementsByTagName("textarea");
-  for (var i = 0; i < textareas.length; i++) {
-    var elem = textareas[i];
+    var widget;
+    if (elem.tagName == "TEXTAREA")
+      widget = require("./textarea.js").textarea;
+    else if (elem.tagName == "DIV" && elem.getAttribute("data-draftdotcloud-widget") == "jsoneditor")
+      widget = jsoneditor;
+    else
+      // invalid
+      continue;
+
     var owner_name = elem.getAttribute("data-draftdotcloud-owner");
     var document_name = elem.getAttribute("data-draftdotcloud-document");
     var api_key = elem.getAttribute("data-draftdotcloud-apikey");
-    if (owner_name && document_name) {
-      console.log("attaching draft.cloud widget to", elem);
-      var widget = require("./textarea.js").textarea;
-      widget = new widget(elem);
-      var client = require("./client.js").Client(
-        owner_name,
-        document_name,
-        api_key,
-        channel,
-        widget,
-        function(doc, msg) {
-          log_receivers.forEach(function(recip) {
-            recip(doc, msg);
-          })
-        }
-      );
+    if (!owner_name || !document_name) continue;
 
-      var cursor_document_name = elem.getAttribute("data-draftdotcloud-cursordocument");
-      if (cursor_document_name) {
-        console.log("...with cursors");
-        var cursorwidget = require("./textarea.js").textarea_cursors;
-        cursorwidget = new cursorwidget(elem);
-        var cursorclient = require("./client.js").Client(
-          owner_name,
-          cursor_document_name,
-          api_key,
-          channel,
-          cursorwidget,
-          function(doc, msg) {
-            log_receivers.forEach(function(recip) {
-              recip(doc, msg);
-            })
-          }
-        );
-
+    console.log("attaching draft.cloud widget to", elem);
+    var client = require("./client.js").Client(
+      owner_name,
+      document_name,
+      api_key,
+      require("./websocket.js"), // or require("./ajax_polling.js")
+      new widget(elem),
+      function(doc, msg) {
+        log_receivers.forEach(function(recip) {
+          recip(doc, msg);
+        })
       }
-    }
+    );
   }
 }
 
---
+++ draft-cloud-widgets/jsoneditor.js
@@ -0,0 +1,116 @@
+var simple_widget = require('./simple_widget.js').simple_widget;
+var jot = require('../jot');
+var jotvals = require('../jot/values.js');
+var jotseqs = require('../jot/sequences.js');
+var jotobjs = require('../jot/objects.js');
+
+// Add CSS and SCRIPT tags for jsoneditor.
+var dist_url = "/static/jsoneditor";
+var elem = document.createElement('link');
+elem.href = dist_url + "/jsoneditor.min.css";
+elem.rel = "stylesheet";
+elem.type = "text/css";
+document.getElementsByTagName('head')[0].appendChild(elem);
+var elem = document.createElement('script');
+elem.src = dist_url + "/jsoneditor.min.js";
+document.getElementsByTagName('head')[0].appendChild(elem);
+
+exports.jsoneditor = function(elem, dist_url) {
+  // Initialize editor in read-only mode.
+  var _this = this;
+  this.editor = new JSONEditor(elem, {
+    mode: 'view',
+    onChange: function() { _this.compute_changes(); }
+  });
+}
+
+exports.jsoneditor.prototype = new simple_widget(); // inherit
+
+exports.jsoneditor.prototype.name = "jsoneditor Widget";
+
+exports.jsoneditor.prototype.get_document = function() {
+  return this.editor.get(); 
+}
+
+exports.jsoneditor.prototype.set_readonly = function(readonly) {
+  this.editor.setMode(readonly ? "view" : "tree");
+  if (!readonly)
+    this.editor.focus();
+}
+
+exports.jsoneditor.prototype.set_document = function(document, patch) {
+  // Calling .set() with the new document will cause the cursor to
+  // reset. Use the editor's undocumented History class to structurally
+  // update the document, if possible.
+
+  if (patch && this.editor.node) {
+    // We're given not only the new document content but also a JOT operation
+    // that represents the change being made. If we can understand the JOT
+    // operation, we can apply it directly to the editor.
+    try {
+      apply(patch, this.editor.node);
+      return; // success
+    } catch (e) {
+      // fail, fall through to below
+      console.log(e);
+    }
+  }
+
+  // Fall back to calling .set().
+  this.editor.set(document);
+}
+
+exports.jsoneditor.prototype.nonfatal_error = function(message) {
+  alert(message);
+}
+
+exports.jsoneditor.prototype.show_status = function(message) {
+  // TODO
+}
+
+function apply(patch, node) {
+  if (patch instanceof jotvals.SET) {
+    console.log(patch, node, patch.value);
+    node.setValue(patch.value, "auto");
+    node.updateDom();
+
+  } else if (patch instanceof jotvals.MATH) {
+    node.setValue(patch.apply(node.getValue()), "auto");
+    node.updateDom();
+
+  } else if (patch instanceof jotobjs.APPLY) {
+    Object.keys(patch.ops).forEach(function(key) {
+      // Get child node.
+      child = node.childs.filter(function (child) {
+        return child.field === key;
+      })[0];
+      if (!child) {
+        child = Node(node.editor);
+        node.appendChild(child);
+      }
+
+      // Apply op to child.
+      apply(patch.ops[key], child);
+    });
+
+  } else if (patch instanceof jotseqs.PATCH && 0) {
+    // TODO: Strings vs arrays.
+    var index = 0;
+    patch.hunks.forEach(function(hunk) {
+      index += hunk.offset;
+      if (hunk.op instanceof jotseqs.MAP) {
+        // Get child node & apply to child.
+        var child = node.childs[index];
+        if (!child)
+          throw "JOT operation refers to invalid index";
+        apply(hunk.op.op, child);
+      } else {
+        throw "unsupported JOT operation inside PATCH " + hunk.op.inspect();
+      }
+      index += hunk.length + hunk.op.get_length_change(hunk.length);
+    });
+
+  } else {
+    throw "unsupported JOT operation " + patch.inspect();
+  }
+}
