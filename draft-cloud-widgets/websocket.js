JoshData/draft.cloud

commit 02aeb109900148f63167154b0458485f3f21be00

send widget state along with revisions and apply widget state when revisions are received so cursors movements occur on peers exactly when the changes that go along with the cursor movements are applied
Although because clients may be in inconsistent states, the cursor data received by peers may not be correct.

draft-cloud-widgets/websocket.js
@@ -111,7 +111,7 @@ function open(owner_name, document_name, api_key, cbobj) {
     last_seen_revision = data.revisions[data.revisions.length-1].id;
   });
 
-  function push(base_revision, patch, cb) {
+  function push(base_revision, patch, widget_state, cb) {
     // The Client class calls this function to submit a JOT operation to
     // the server. The server will record an *uncommitted* Revision and
     // send that back in the socket callback function.
@@ -132,6 +132,7 @@ function open(owner_name, document_name, api_key, cbobj) {
       document: document_id,
       base_revision: base_revision,
       patch: patch.toJSON(),
+      widget_state: widget_state,
       comment: null,
       userdata: null
     }, function(data) {

draft-cloud-api/live.js
@@ -195,6 +195,11 @@ exports.init = function(io, sessionStore, settings) {
       // Load the document.
       var doc_state = socket.open_documents[data.document];
 
+      // Add the widget state to the userdata.
+      var userdata = (data.userdata || {});
+      userdata.widget_state = { };
+      userdata.widget_state[socket.id] = data.widget_state;
+
       // Find the base revision. If not specified, it's the current revision.
       models.Revision.from_uuid(doc_state.document, data.base_revision, function(base_revision) {
         routes.make_revision(
@@ -204,7 +209,7 @@ exports.init = function(io, sessionStore, settings) {
           op,
           doc_state.doc_pointer,
           data.comment,
-          data.userdata,
+          userdata,
           {
             _status: null,
             status: function(code) { this._status = code; return this; },

draft-cloud-widgets/ajax_polling.js
@@ -94,7 +94,7 @@ exports.open = function(owner_name, document_name, api_key, cbobj) {
       poll_remote_changes(); // kick it off
 
       // Define the push function to send new changes to the server.
-      function push(base_revision, patch, cb) {
+      function push(base_revision, patch, widget_state, cb) {
         $.ajax({
           url: document_url(owner_name, document_name, "content"),
           method: "PATCH",

draft-cloud-widgets/client.js
@@ -250,6 +250,18 @@ exports.Client = function(owner_name, document_name, api_key, channel, widget, l
     if (!patch.isNoOp())
       widget.merge_remote_changes(patch);
 
+
+    // Apply any ephemeral widget states that came through with revisions,
+    // execept for a revision that is from our own changes --- we're not
+    // a "peer".
+    remote_changes.forEach(function(revision) {
+      if (revision == seen_ours) return; // it's a revision we sent
+      if (revision.userdata && typeof revision.userdata.widget_state == "object") {
+        for (let peerid in revision.userdata.widget_state)
+          widget.on_peer_state_updated(peerid, revision.author, revision.userdata.widget_state[peerid]);
+      }
+    });
+
     // Update Document State
     // =====================
     remote_changes = [];
@@ -283,7 +295,8 @@ exports.Client = function(owner_name, document_name, api_key, channel, widget, l
         
         widget.status("saving");
 
-        channel_methods.push(widget_base_revision, patch, function(err, revision) {
+        channel_methods.push(widget_base_revision, patch, widget.get_ephemeral_state(),
+          function(err, revision) {
           // We've gotten back the revision ID for the patch we
           // submitted. Remember it for later when we process
           // remote changes, so we can identify when we see
@@ -305,14 +318,24 @@ exports.Client = function(owner_name, document_name, api_key, channel, widget, l
           waiting_for_local_change_to_save = false;
           merge_remote_changes();
         })
+      
+      } else {
+        // Check if the ephemeral_state changed. The ephemeral state
+        // includes cursor information. Since the ephemeral state is
+        // propagated faster than document changes, we need to prevent
+        // cursor movements from propagating ahead of the changes that
+        // the movement caused (i.e. typing text will cause the cursor
+        // to move forward but the cursor movement on other clients should
+        // occur along with the insertion of text, not before (or after).)
+        // So we don't send ephemeral state if we have unsaved changes
+        // in the pipeline. The state is, instead, sent along with the
+        // revision.
+        var es = widget.get_ephemeral_state();
+        if (jot.cmp(ephemeral_state, es) != 0) {
+          channel_methods.send_state(es);
+          ephemeral_state = es;
+        }
       }
-    }
-
-    // Check if the ephemeral_state changed.
-    var es = widget.get_ephemeral_state();
-    if (jot.cmp(ephemeral_state, es) != 0) {
-      channel_methods.send_state(es);
-      ephemeral_state = es;
     }
   }
 

commit 899a356feed8a9d221354d25e3f256fe5309abc2

fix for reconnecting after network break broken in part by 02aeb109900148f63167154b0458485f3f21be00

draft-cloud-widgets/websocket.js
@@ -107,6 +107,7 @@ function open(owner_name, document_name, api_key, cbobj) {
 
   socket.on('new-revisions', function (data) {
     if (data.document != document_id) return; // for a different open-document stream
+    if (data.revisions.length == 0) return; // no data?
     cbobj.pull(data.revisions);
     last_seen_revision = data.revisions[data.revisions.length-1].id;
   });
@@ -123,7 +124,7 @@ function open(owner_name, document_name, api_key, cbobj) {
       // If the socket isn't open, just queue a call to this function
       // with the same arguments for when the socket reconnects.
       socket_reconnect_actions.push(function() {
-        push(base_revision, patch, cb);
+        push(base_revision, patch, widget_state, cb);
       })
       return;
     }