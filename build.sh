43c766401626de6071c4080d59cb928296f5cf7c
Joshua Tauberer <jt@occams.info>
2018-05-31 08:37:36 -0400

    api docs fixes

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Build client-side widgets library.
 node_modules/browserify/bin/cmd.js -d widgets/draftdotcloud.js -o public_html/draftdotcloud.js
 
-
+# Build API documentation.
+node_modules/spectacle-docs/bin/spectacle.js -t public_html/apidocs/v1 backend/swagger.yaml
