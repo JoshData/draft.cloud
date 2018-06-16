839fcdd7911b79f71c302c0ac645c0081aaf1de4
Joshua Tauberer <jt@occams.info>
2018-06-16 11:20:01 -0400

    add nicer quill editor content styling - margins and a better font

--- build.sh
+++ build.sh
@@ -11,3 +11,5 @@ FONTDIR=public_html/static/fonts/google
 rm -rf $FONTDIR
 mkdir -p $FONTDIR
 node_modules/google-font-installer/cli.js download -d $FONTDIR Macondo
+node_modules/google-font-installer/cli.js download -d $FONTDIR "IBM Plex Sans"
+node_modules/google-font-installer/cli.js download -d $FONTDIR "IBM Plex Mono"
